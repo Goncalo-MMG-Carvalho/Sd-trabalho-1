a318e6904f96e7802306c8a2b814476dca6aeb6b
Sebastiao <s.jeronimo2002@gmail.com>
2024-04-12 19:30:07 +0100

    added the server files

---
+++ src/tukano/servers/java/JavaUsers.java
@@ -0,0 +1,150 @@
+package lab2.servers.java;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import tukano.api.java.Result;
+import tukano.api.java.Result.ErrorCode;
+import tukano.api.User;
+import tukano.api.java.Users;
+
+public class JavaUsers implements Users {
+	private final Map<String,User> users = new HashMap<>();
+
+	private static Logger Log = Logger.getLogger(JavaUsers.class.getName());
+
+	@Override
+	public Result<String> createUser(User user) {
+		Log.info("createUser : " + user);
+		
+		// Check if user data is valid
+		if(user.userId() == null || user.pwd() == null || user.displayName() == null || user.email() == null) {
+			Log.info("User object invalid.");
+			return Result.error( ErrorCode.BAD_REQUEST);
+		}
+		
+		// Insert user, checking if name already exists
+		if( users.putIfAbsent(user.userId(), user) != null ) {
+			Log.info("User already exists.");
+			return Result.error( ErrorCode.CONFLICT);
+		}
+		return Result.ok( user.userId() );
+	}
+
+	@Override
+	public Result<User> getUser(String userId, String pwd) {
+		Log.info("getUser : user = " + userId + "; pwd = " + pwd);
+		
+		// Check if user is valid
+		if(userId == null || pwd == null) {
+			Log.info("Name or Password null.");
+			return Result.error( ErrorCode.BAD_REQUEST);
+		}
+		
+		User user = users.get(userId);			
+		// Check if user exists 
+		if( user == null ) {
+			Log.info("User does not exist.");
+			return Result.error( ErrorCode.NOT_FOUND);
+		}
+		
+		//Check if the password is correct
+		if( !user.pwd().equals( pwd)) {
+			Log.info("Password is incorrect.");
+			return Result.error( ErrorCode.FORBIDDEN);
+		}
+		
+		return Result.ok(user);
+	}
+
+	@Override
+	public Result<User> updateUser(String userId, String pwd, User newUser) { // TODO DONE
+		
+		Log.info("getUser : user = " + userId + "; pwd = " + pwd);
+		
+		// Check if user is valid
+		if(userId == null || pwd == null) {
+			Log.info("Name or Password null.");
+			return Result.error( ErrorCode.BAD_REQUEST);
+		}
+		
+		if(!userId.equals(newUser.userId())) {
+			Log.info("User id can't be changed");
+			return Result.error( ErrorCode.BAD_REQUEST);
+		}
+		
+		User currUser = users.get(userId);			
+		// Check if user exists 
+		if( currUser == null ) {
+			Log.info("User does not exist.");
+			return Result.error( ErrorCode.NOT_FOUND);
+		}
+		
+		//Check if the password is correct
+		if( !currUser.pwd().equals(pwd) ) {
+			Log.info("Password is incorrect.");
+			return Result.error( ErrorCode.FORBIDDEN);
+		}
+		
+		users.replace(userId, newUser);
+		
+		//return Result.error( ErrorCode.NOT_IMPLEMENTED);
+		return Result.ok(newUser);
+	}
+
+	@Override
+	public Result<User> deleteUser(String userId, String pwd) { //TODO DONE
+
+		Log.info("delete user : user = " + userId + "; pwd = " + pwd);
+		
+		// Check if user is valid
+		if(userId == null || pwd == null) {
+			Log.info("Name or Password null.");
+			return Result.error( ErrorCode.BAD_REQUEST);
+		}
+		
+		User user = users.get(userId);			
+		// Check if user exists 
+		if( user == null ) {
+			Log.info("User does not exist.");
+			return Result.error( ErrorCode.NOT_FOUND);
+		}
+		
+		//Check if the password is correct
+		if( !user.pwd().equals( pwd)) {
+			Log.info("Password is incorrect.");
+			return Result.error( ErrorCode.FORBIDDEN);
+		}
+		
+		users.remove(userId);
+		
+		
+		//return Result.error( ErrorCode.NOT_IMPLEMENTED);
+		return Result.ok(user);
+	}
+
+	@Override
+	public Result<List<User>> searchUsers(String pattern) { //TODO DONE
+		Log.info("search users: pattern = " + null);
+		
+		if(pattern == null) {
+			Log.info("pattern is null.");
+			return Result.error( ErrorCode.BAD_REQUEST);
+		}
+		
+		List<User> matchedUsers = new ArrayList<>();
+		String PATTERN = pattern.toLowerCase();
+
+		users.forEach((userId, user) -> {
+			if (userId.toLowerCase().contains(PATTERN)) {
+              matchedUsers.add(user);
+			}
+		});
+		
+        return Result.ok(matchedUsers);
+		//return Result.error( ErrorCode.NOT_IMPLEMENTED);
+	}
+}
